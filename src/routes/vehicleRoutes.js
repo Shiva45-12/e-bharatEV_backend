46722bd77556dfce2dae6001d416aff8c30cd716
SHIVA VISHWAKARMA <raw66010@gmail.com>
2026-07-07 15:12:08 +0530

    all done

--- src/routes/vehicleRoutes.js
+++ src/routes/vehicleRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { registerVehicle, getUserVehicles } = require('../controllers/vehicleController');
-const { protect } = require('../middlewares/authMiddleware'); // assuming there's an authMiddleware for users
+const { protectUser } = require('../middlewares/authMiddleware'); 
 
-router.post('/', protect, registerVehicle);
-router.get('/', protect, getUserVehicles);
+router.post('/', protectUser, registerVehicle);
+router.get('/', protectUser, getUserVehicles);
 
 module.exports = router;
